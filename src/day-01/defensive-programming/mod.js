915a455d6910855f857171c054a5a0e86fbbfb72
Maurici Abad <maurici.abad@glovoapp.com>
2021-07-13 16:50:35 +0200

    Fix mod tests

--- src/day-01/defensive-programming/mod.js
+++ src/day-01/defensive-programming/mod.js
@@ -2,7 +2,7 @@
  * Modulo operation
  * @param {number} n
  * @param {number} d
- * @returns {number} Returns NaN when d is negative or one of the params is not a number
+ * @returns {number} Returns NaN when d is 0 or one of the params is not a number
  */
 export function mod(n, d) {
   return NaN
