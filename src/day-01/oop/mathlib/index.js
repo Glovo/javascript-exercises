f419a2b0510425b2152c6c85bc535f02fa1311b8
peoplenarthax <angel.paredes@glovoapp.com>
2021-07-13 15:28:06 +0200

    Move math files

--- src/day-01/oop/mathlib/index.js
+++
@@ -1,5 +0,0 @@
-// Define a Math class which
-// 1- It is initialize with a number, every operation mutates that number
-// 2- We can get the given number
-// 3- We define the next operations:
-//    3.1 - add, divide, mult, substract.
